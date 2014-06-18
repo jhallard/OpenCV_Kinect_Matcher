96e97520bfb7f4289515ba5b5ffb9dcab62d7600
jhallard <jhallard@ucsc.edu>
2014-06-18 14:28:45 -0700

    changed back to old program , was getting some seg faults out of no where from the old progprogram

--- src/connect_kinect.cpp
+++ src/connect_kinect.cpp
@@ -22,35 +22,44 @@
 #include "opencv2/highgui/highgui.hpp"
 
 #include <vector>
+#include <string>
 
 #define WINDOW_NAME "Kinect RGB Image"
+#define NP 100
 using namespace std;
 using namespace cv;
 
-
+bool computeKeyPoints(string filename);
 void imageCallback (const sensor_msgs::Image::ConstPtr& img);
+bool changeDetectorExtractor(string type);
 
-  Mat queryImg;
+cv::Ptr<cv::FeatureDetector> detector;
+cv::Ptr<cv::DescriptorExtractor> extractor;
+
+Mat queryImg;
 vector<KeyPoint> queryKeypoints;
 Mat queryDescriptors;
+vector<string> filenames;
+int currentfile = 0;
 
 int main(int argc, char **argv)
 {
 
   ros::init(argc, argv, "listener");
 
-  queryImg = imread("../pics/can.jpg");
+  for(int i = 1; i < argc; i++)
+  {
+    string x("../pics/hands/");
+    filenames.push_back(x+argv[i]);
+  }
+  cv::initModule_nonfree();
+  changeDetectorExtractor("MSER");
 
-  if(queryImg.empty())
-    std::cout << "fuck";
-
-  // Detect keypoints in both images.
-  SiftFeatureDetector detector(10);
-  detector.detect(queryImg, queryKeypoints);
-
-  SiftDescriptorExtractor extractor;
-  extractor.compute(queryImg, queryKeypoints, queryDescriptors);
-
+  if(!computeKeyPoints(filenames[currentfile]))
+  {
+    std::cout << "error loading file";
+    return -1;
+  }
 
   cv::namedWindow(WINDOW_NAME);
 
@@ -65,30 +74,61 @@ int main(int argc, char **argv)
   return 0;
 }
 
+bool changeDetectorExtractor(string type)
+{
+  detector = cv::FeatureDetector::create(type);
+  if(detector==NULL)
+    std::cout<<"detector NULL"<<std::endl;
+
+  extractor = cv::DescriptorExtractor::create("SURF");
+  if(extractor==NULL){std::cout<<"descriptorExtractor NULL"<<std::endl;}
+}
+
+bool computeKeyPoints(string filename)
+{
+  queryImg = imread(filename);//[0]);
+  //queryImg = imread(argv[1]);
+  //queryImg = imread("../pics/can.jpg");
+
+  if(queryImg.empty())
+  {
+    return false;
+  }
+
+  // Detect keypoints in both images.
+  //SiftFeatureDetector detector(NP);
+  detector->detect(queryImg, queryKeypoints);
+
+  //SiftDescriptorExtractor extractor;
+  extractor->compute(queryImg, queryKeypoints, queryDescriptors);
+
+  return true;
+}
+
 void imageCallback (const sensor_msgs::Image::ConstPtr& img)
 {
   cv_bridge::CvImagePtr cv_ptr;
 
-  try {
-
-        cv_ptr = cv_bridge::toCvCopy(*img, sensor_msgs::image_encodings::BGR8); //enc::RGB8 also used
-
-    } catch (cv_bridge::Exception& e) {
-
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
+  try 
+  {
+    cv_ptr = cv_bridge::toCvCopy(*img, sensor_msgs::image_encodings::BGR8); //enc::RGB8 also used
+  }
+  catch (cv_bridge::Exception& e) 
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
 
     cv::Mat trainImg = cv_ptr->image;
-    SiftFeatureDetector detector(10);
+    //SiftFeatureDetector detector(NP);
     vector<KeyPoint> trainKeypoints;
-    detector.detect(trainImg, trainKeypoints);
+    detector->detect(trainImg, trainKeypoints);
 
-    SiftDescriptorExtractor extractor;
+    //SiftDescriptorExtractor extractor;
     Mat trainDescriptors;
-    extractor.compute(trainImg, trainKeypoints, trainDescriptors);
+    extractor->compute(trainImg, trainKeypoints, trainDescriptors);
 
-    cv::FlannBasedMatcher matcher;
+    cv::FlannBasedMatcher matcher (new cv::flann::KDTreeIndexParams(4), new cv::flann::SearchParams(64));
     vector<DMatch> matches;
     matcher.match(queryDescriptors, trainDescriptors, matches);
 
@@ -98,5 +138,18 @@ void imageCallback (const sensor_msgs::Image::ConstPtr& img)
 
 
     //cv::imshow(WINDOW_NAME, cv_ptr->image);
-    cv::waitKey(2);
+    char key = cv::waitKey(2);
+    switch(key)
+    {
+      case 'n' :
+        currentfile++;
+        if(currentfile == filenames.size())
+          currentfile = 0;
+
+        if(!computeKeyPoints(filenames[currentfile]))
+          ROS_ERROR("Could not load new file in image callback");
+          return;
+
+      break;
+    }
 }
