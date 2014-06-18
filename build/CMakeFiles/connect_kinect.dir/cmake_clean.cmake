e2a663c1aec42194c301e6ab62d1f393629f83eb
jhallard <jhallard@ucsc.edu>
2014-06-17 17:29:05 -0700

    Initial commit, the program compiles and works decently but needs calibration

---
+++ build/CMakeFiles/connect_kinect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/connect_kinect.dir/src/connect_kinect.cpp.o"
+  "connect_kinect.pdb"
+  "connect_kinect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/connect_kinect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotics/Desktop/openCV_Related/opencv-kinect
+# Build directory: /home/robotics/Desktop/openCV_Related/opencv-kinect/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
